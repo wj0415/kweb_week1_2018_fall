4e1886e7c7b3b561390996acfd60a1260ac88fcb
WhoJung Kim <wj0415@korea.ac.kr>
2018-09-30 19:28:48 +0900

    My first commit

---
+++ cal1.js
@@ -0,0 +1,15 @@
+const calc = {};
+calc.add = (a, b) => a + b;
+calc.sub = (a, b) => a - b;
+calc.mul = (a, b) => a * b;
+calc.div = function(a, b) {
+  if (b == 0) {
+    return new Error("zero divider error.");
+  }
+  return a / b;
+};
+calc.getE = function() {
+  return Math.E;
+};
+
+module.exports = calc;
